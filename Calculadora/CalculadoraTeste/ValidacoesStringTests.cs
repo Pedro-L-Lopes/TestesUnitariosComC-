4ab7c79a9d419fe2517f159ee55a8a26ade9443c
Pedro Lucas <pedrolucas122737@gmail.com>
2023-12-29 12:16:38 -0300

    Implementando validações de string

---
+++ Calculadora/CalculadoraTeste/ValidacoesStringTests.cs
@@ -0,0 +1,26 @@
+﻿using Calculadora.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTeste
+{
+    public class ValidacoesStringTests
+    {
+        private ValidacoesString _validacoes;
+
+        public ValidacoesStringTests()
+        {
+            _validacoes = new ValidacoesString();
+        }
+
+        [Fact]
+        public void ContarTotalDeCaracteresNaString()
+        {
+            Assert.Equal(5, _validacoes.ContarCaracteres("Caval"));
+        }
+    }
+
+}
