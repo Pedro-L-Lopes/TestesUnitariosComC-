6bd7fd6420280c3e4968de4d94d58181c77b7a92
Pedro Lucas <pedrolucas122737@gmail.com>
2023-12-29 11:55:17 -0300

    Implementando testes na calculadora

---
+++ Calculadora/Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+        public int Dividir(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Calculadora/CalculadoraTeste/CalculadoraTests.cs
@@ -0,0 +1,47 @@
+using Calculadora.Services;
+
+namespace CalculadoraTeste
+{
+    public class CalculadoraTests
+    {
+
+        private CalculadoraImp _calc;
+
+        public CalculadoraTests()
+        {
+            _calc = new CalculadoraImp();
+        }
+
+        [Fact]
+        public void Somar()
+        {
+            // Arrange
+            int num1 = 10;
+            int num2 = 10;
+
+            // Act
+            int resultadoSoma = _calc.Somar(num1, num2);
+
+            // Assert
+            Assert.Equal(20, resultadoSoma);
+        }
+
+        [Fact]
+        public void Subtrair()
+        {
+            Assert.Equal(0, _calc.Subtrair(10, 10));
+        }
+
+        [Fact]
+        public void Multiplicar()
+        {
+            Assert.Equal(100, _calc.Multiplicar(10, 10));
+        }
+
+        [Fact]
+        public void Dividir()
+        {
+            Assert.Equal(1, _calc.Dividir(10, 10));
+        }
+    }
+}
